4b95c3aa4732866e38ee35534951a0fdc3714a5d
Kiseleva_EV <kiseleva_elena92@mail.ru>
2021-08-06 21:01:46 +0400

    empty project

---
+++ src/test/java/ru/irlix/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.irlix.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
